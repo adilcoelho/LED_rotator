aeac4f57faf8a5c1c968c4f76f735429d02648f8
Adil Coelho <adilcoelho@gmail.com>
2019-05-21 14:32:55 -0300

    Fix fairy tail 666

--- ex2.vhd
+++ ex2.vhd
@@ -4,15 +4,16 @@ use ieee.numeric_std.all;
 
 ENTITY ex2 IS
 	GENERIC(
-				time_m : integer := 1;
-				freq_clk: integer := 1e3;
+				time_m : integer := 666;
+				freq_clk: integer := 50e6;
 				NLED: integer := 10
 	);
 	PORT(
 		  clk : IN STD_LOGIC;
 		  speed :  IN STD_LOGIC_VECTOR(2 DOWNTO 0);
 		  reverse : IN STD_LOGIC;
-		  tail_size_control : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+		  tail_size_control_plus : IN STD_LOGIC;
+		  tail_size_control_minus : IN STD_LOGIC;
 		  led : OUT STD_LOGIC_VECTOR(NLED-1 DOWNTO 0)
 	);
 END ENTITY;
@@ -20,8 +21,8 @@ END ENTITY;
 ARCHITECTURE arch OF ex2 IS
 	COMPONENT debounce IS
 		GENERIC(
-				time_ms : integer := 1;
-				freq_clk: integer := 1e3	     
+				time_ms : integer := 100;
+				freq_clk: integer := 50e6	     
 		);
 		PORT(
   		  button : in std_logic;
@@ -34,37 +35,45 @@ SIGNAL reverse_out, sentido, rabomais, rabomenos: std_LOGIC;
 SIGNAL N : integer;
 BEGIN
 	debounce_reverse : debounce port map(button => reverse, clk => clk, debounced_out => reverse_out);
-	debounce_rabomais : debounce port map(button => tail_size_control(1), clk => clk, debounced_out => rabomais); -- tail...(1) aumenta rabo
-	debounce_rabomenos : debounce port map(button => tail_size_control(0), clk => clk, debounced_out => rabomenos); -- tail...(0) diminui rabo
+	debounce_rabomais : debounce port map(button => tail_size_control_plus, clk => clk, debounced_out => rabomais); --  aumenta rabo
+	debounce_rabomenos : debounce port map(button => tail_size_control_minus, clk => clk, debounced_out => rabomenos); -- diminui rabo
 	
 --MOVIMENTO DA TRILHA
 PROCESS(clk,reverse_out, rabomais, rabomenos)
 	VARIABLE speed_count : integer := to_integer(unsigned(speed));
-	VARIABLE counter_max: integer := (time_m/2*speed_count+2) * (freq_clk /1e3);
+	VARIABLE counter_max: integer := (time_m/(2*speed_count+2)) * (freq_clk /1e3);
 	VARIABLE counter: integer := 0;
 	VARIABLE sentido: STD_LOGIC := '0';
 	VARIABLE vetor_leds: UNSIGNED(NLED-1 downto 0) := (others => '0');
 	VARIABLE posicao : integer  range NLED-1 downto 0  := 0;
 	VARIABLE tamanho_rabo: integer range NLED - 2 downto 0 := 0;
+	VARIABLE apertado: std_logic := '0';
 	BEGIN
-	IF rising_edge(reverse_out) THEN
+	IF falling_edge(reverse_out) THEN
 	  sentido := not(sentido);
 	END IF;
 
 	
 	IF rising_edge(clk) THEN
 
-		IF rabomais = '1' THEN
+		IF rabomais = '1' and rabomenos = '1' THEN
+			apertado := '0';
+		END IF;
+
+		IF rabomenos = '0' and apertado = '0' THEN
+			apertado := '1';
+			IF tamanho_rabo > 0 THEN
+				tamanho_rabo := tamanho_rabo - 1;
+			END IF;
+		END IF;
+
+		IF rabomais = '0' and apertado = '0' THEN
+			apertado := '1';
 			IF tamanho_rabo < NLED-2 THEN
 				tamanho_rabo := tamanho_rabo + 1;
 			END IF;
 		END IF;
 
-		IF rabomenos = '1' THEN
-			IF tamanho_rabo > 0 THEN
-				tamanho_rabo := tamanho_rabo - 1;
-			END IF;
-		END IF;
 		
 		vetor_leds := (others => '0');
 		vetor_leds(tamanho_rabo downto 0) := (others => '1');
---
+++ output_files/ex2.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 13.1.0 Build 162 10/23/2013 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP3C16F484) Path("H:/UTFPR/8Periodo/logRec/LABALGO/vhdl/output_files/") File("ex2.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
